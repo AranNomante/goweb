12d11aadd7bd86eff07e88f9a003b872fd079ea0
Altuğ Ceylan <altug.ceylan@dt.net.tr>
2021-06-04 13:07:51 +0300

    route handling basic

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter,r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w,"<h1>Welcome to site</h1>")
+	if r.URL.Path == "/"{
+		fmt.Fprintf(w,"<h1>Welcome to site</h1>")
+	}else if r.URL.Path == "/contact"{
+		fmt.Fprintf(w,"To get in touch, please send an email to <a href=\"mailto:altug.ceylan@dt.net.tr\">Altug Ceylan</a>")
+	}
 }
 func main(){
 	http.HandleFunc("/",handlerFunc)
