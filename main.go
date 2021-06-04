99b975c64fe8848d0ab87d4422651a7ce2483a65
Altuğ Ceylan <altug.ceylan@dt.net.tr>
2021-06-04 14:26:32 +0300

    notfound

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter,r *http.Request){
 		fmt.Fprintf(w,"<h1>Welcome to site</h1>")
 	}else if r.URL.Path == "/contact"{
 		fmt.Fprintf(w,"To get in touch, please send an email to <a href=\"mailto:altug.ceylan@dt.net.tr\">Altug Ceylan</a>")
+	}else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w,"Page not found, please send an email to <a href=\"mailto:altug.ceylan@dt.net.tr\">Altug Ceylan if you keep...</a>")
 	}
 }
 func main(){
